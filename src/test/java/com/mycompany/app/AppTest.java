4af98d7cf0e645eab08267b1376fed92a840a471
ranjith318 <99415593+ranjith318@users.noreply.github.com>
2022-03-15 21:58:14 +0530

    testcases added

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -13,7 +13,27 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue3()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue4()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue5()
     {
         assertTrue( true );
     }
